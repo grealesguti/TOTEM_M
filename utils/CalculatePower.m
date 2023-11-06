af1950a881fa2b77f480c8720725043f5af63201
greales <g.realesguti@gmail.com>
2023-11-06 19:00:19 +0100

    add: element dimension in power calculation

--- utils/CalculatePower.m
+++ utils/CalculatePower.m
@@ -1,10 +1,13 @@
 function Power = CalculatePower(reader,mesh,solver)
     Power=0;
+    mesh_elements = mesh.retrieveElementalSelection(reader.MeshEntityName);
+    etype=mesh.data.ElementTypes{mesh_elements(1)};
+    dim = mesh.retrieveelementdimension(etype); 
     integrationfunction = @(natural_coordinates, element_coordinates, Tee, Vee, element_material_index, reader, mesh, etype,xx) IntegrationPower(natural_coordinates, element_coordinates, Tee, Vee, element_material_index, reader, mesh, etype,xx);
     mesh_elements = mesh.retrieveElementalSelection(reader.MeshEntityName);
     for i=1:length(mesh_elements)
         element_tag= mesh_elements(i);
-        [Element_Power,element_dof_indexes]=GeneralGaussIntegration(3, 14, element_tag, mesh, solver.soldofs,reader,mesh.data.ElementTypes{element_tag},integrationfunction);
+        [Element_Power,element_dof_indexes]=GeneralGaussIntegration(dim, 3, element_tag, mesh, solver.soldofs,reader,mesh.data.ElementTypes{element_tag},integrationfunction);
         Power=Power+Element_Power;
     end
 end
